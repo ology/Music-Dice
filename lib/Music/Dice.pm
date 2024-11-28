f164f6ad1e58476673d32a74830260cc4d41fa08
Gene Boggs <gene@ology.net>
2024-11-27 22:09:46 -0800

    Constraints

--- lib/Music/Dice.pm
+++ lib/Music/Dice.pm
@@ -672,7 +672,7 @@ sub rhythmic_phrase {
   $result = $md->rhythmic_phrase_constrained->roll;
 
 Returns a constrained rhythmic phrase, given the number of
-B<rhythmic_phrase_constraints>.
+B<rhythmic_phrase_constraints> (number of rhythmic values).
 
 =cut
 
